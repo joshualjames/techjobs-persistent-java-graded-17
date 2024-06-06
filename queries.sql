80bfacabe1fb00e039a3d3efbd3e5b30b1112a34
Joshua James <josh452063@gmail.com>
2024-06-05 21:15:57 -0500

    completed tasks 1 and 2.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,8 @@
 --Part 1
+--Columns: id(int), employer(varchar), name(varchar), skills(varchar)
 
 --Part 2
-
+--SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
 
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank(message = "Location is required")
+    @Size(max = 50, message = "Location must be 50 characters or less")
+    private String location;
+
+    public Employer() {}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
